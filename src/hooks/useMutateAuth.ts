0c68667e7ea2f3c1c03ad5985882f59a811d4cfb
Minimalist-00 <kouki4122@icloud.com>
2023-06-30 18:48:22 +0900

    Update useMutateAuth.ts

--- src/hooks/useMutateAuth.ts
+++ src/hooks/useMutateAuth.ts
@@ -4,3 +4,57 @@ import { useMutation } from '@tanstack/react-query'
 import useStore from '../store'
 import { Credential } from '../types'
 import { useError } from '../hooks/useError'
+
+export const useMutateAuth = () => {
+  const navigate = useNavigate()
+  const resetEditedTask = useStore((state) => state.resetEditedTask) // storeのresetEditedTaskを取得
+  const { switchErrorHandling } = useError()
+  const loginMutation = useMutation(
+    async (
+      user: Credential // 引数でEmailとPasswordを受け取る
+    ) => await axios.post(`${process.env.REACT_APP_API_URL}/login`, user),
+    {
+      onSuccess: () => {
+        navigate('/todo')
+      },
+      onError: (err: any) => {
+        if (err.response.data.message) {
+          // csfrのエラーだけは.messageの階層にある
+          switchErrorHandling(err.response.data.message)
+        } else {
+          switchErrorHandling(err.response.data)
+        }
+      },
+    }
+  )
+  const signupMutation = useMutation(
+    async (user: Credential) =>
+      await axios.post(`${process.env.REACT_APP_API_URL}/signup`, user),
+    {
+      onError: (err: any) => {
+        if (err.response.data.message) {
+          switchErrorHandling(err.response.data.message)
+        } else {
+          switchErrorHandling(err.response.data)
+        }
+      },
+    }
+  )
+  const logoutMutation = useMutation(
+    async () => await axios.post(`${process.env.REACT_APP_API_URL}/logout`),
+    {
+      onSuccess: () => {
+        resetEditedTask()
+        navigate('/')
+      },
+      onError: (err: any) => {
+        if (err.response.data.message) {
+          switchErrorHandling(err.response.data.message)
+        } else {
+          switchErrorHandling(err.response.data)
+        }
+      },
+    }
+  )
+  return { loginMutation, signupMutation, logoutMutation }
+}
