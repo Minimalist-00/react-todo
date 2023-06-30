53ff3b307ff45ac4b8685f721d90e81c5687faac
Minimalist-00 <kouki4122@icloud.com>
2023-06-30 16:37:02 +0900

    storeのstate設定と、諸々

---
+++ src/store/index.ts
@@ -0,0 +1,23 @@
+import { create } from 'zustand'
+
+type EditedTask = {
+  id: number
+  title: string
+}
+
+type State = {
+  editedTask: EditedTask
+  updateEditedTask: (payload: EditedTask) => void // payloadは引数
+  resetEditedTask: () => void
+}
+
+const useStore = create<State>((set) => ({
+  editedTask: { id: 0, title: '' }, // 初期値
+  updateEditedTask: (payload) =>
+    set({
+      editedTask: payload,
+    }),
+  resetEditedTask: () => set({ editedTask: { id: 0, title: '' } }),
+}))
+
+export default useStore
